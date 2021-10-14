a70cca911d3a0d184e55cd8a7528880e48f4e0a2
DSoutter <duncan.soutter@gmail.com>
2021-10-14 11:54:44 +0100

    controllers working

--- src/main/java/com/codeclan/example/bookingLab/controllers/CustomerController.java
+++ src/main/java/com/codeclan/example/bookingLab/controllers/CustomerController.java
@@ -1,4 +1,33 @@
 package com.codeclan.example.bookingLab.controllers;
 
+import com.codeclan.example.bookingLab.models.Customer;
+import com.codeclan.example.bookingLab.repositories.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
 public class CustomerController {
+
+    @Autowired
+    CustomerRepository customerRepository;
+
+    @GetMapping(value = "/customers")
+    public ResponseEntity<List<Customer>> getAllCustomers(){
+        return new ResponseEntity<>(customerRepository.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/customers/{id}")
+    public ResponseEntity getCustomer (@PathVariable Long id){
+        return new ResponseEntity<>(customerRepository.findById(id), HttpStatus.OK);
+    }
+
+    @PostMapping(value = "/customers")
+    public ResponseEntity<Customer> postCustomer(@RequestBody Customer customer){
+        customerRepository.save(customer);
+        return new ResponseEntity<>(customer, HttpStatus.CREATED);
+    }
 }
